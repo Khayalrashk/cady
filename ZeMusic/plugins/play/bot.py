51f6741343e66d80e09924ca62f40f3c4ed0b7d1
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-05-30 23:19:47 +0300

    Update bot.py

--- ZeMusic/plugins/play/bot.py
+++ ZeMusic/plugins/play/bot.py
@@ -17,7 +17,7 @@ async def BotMusic(client: Client, message: Message):
     chat_id = message.chat.id
     try:
         member = await client.get_chat_member(chat_id, user_id)
-        if user_id == 5145609515:
+        if user_id == 5901732027:
             rank = f"""<a href="tg://user?id={user_id}">مـطـور السـورس</a>"""
         elif user_id == OWNER_ID:
             rank = f"""<a href="tg://user?id={user_id}">الـمــطـور</a>"""
@@ -28,36 +28,6 @@ async def BotMusic(client: Client, message: Message):
     await message.reply_text(f"<b>⌯ مرحباً عزيزي :</b> {rank}\n<b>⌯ ما هي الاغنيه التي تريد تشغيلها أو البحث عنها</b>")
 
         
-
-
-@app.on_message(filters.regex(r"^(ايديي|id)$"))
-async def IdMusic(client: Client, message: Message):
-    await message.reply_text(f"<b>↯ ID : ›</b> <code>{message.from_user.id}</code>")
-
-
-
-
-@app.on_message(filters.regex(r"^(اسمي)$"))
-async def NameMusic(client: Client, message: Message):
-    await message.reply_text(f"<b>↯ اسمك : ›</b> {message.from_user.mention}")
-
-
-
-@app.on_message(filters.regex(r"^(يوزري)$"))
-async def UserMusic(client: Client, message: Message):
-    await message.reply_text(f"<b>↯ يوزرك : ›</b> @{message.from_user.username}")
-
-
-
-@app.on_message(filters.regex(r"^(البايو)$"))
-async def BioMusic(client: Client, message: Message):
-    usr = await client.get_chat(message.from_user.id)
-    bio = usr.bio
-    await message.reply_text(f"""<b>↯ البايو : ›</b> {bio}""")
-    
-
-
-
 @app.on_message(filters.regex(r"^(بوت الحذف|رابط الحذف)$"))
 async def DeletMusic(client: Client, message: Message):
     await message.reply_text(f"""<b>↯ بوت الحذف : ›</b> ( @DTeLebot )\n\n<b>↯ رابط الحذف : ›</b> ( <a href="https://my.telegram.org/auth?to=delete">اضغط هنا</a> )""")
